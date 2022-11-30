fc74aa691912924a17a1d130afbd28bdfcb1a16d
Toan Nguyen <woodpecker146@gmail.com>
2022-11-30 08:46:18 +0700

    chore(web-erp): remove config because modules is built to commonjs

--- packages/web-erp/.web/remix.config.js
+++ packages/web-erp/.web/remix.config.js
@@ -3,7 +3,6 @@ const { registerWebRoutes } = require('@roxavn/core/server');
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  serverDependenciesToBundle: [/^@roxavn/],
   watchPaths: ['./src/**/*'],
   routes: (defineRoutes) => registerWebRoutes(defineRoutes),
 };
