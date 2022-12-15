569f78b5d6752459c657a6cebab45a0c10901a26
Toan Nguyen <woodpecker146@gmail.com>
2022-12-15 14:38:37 +0700

    chore(module-user): move errors file

---
+++ packages/module-user/src/share/errors.ts
@@ -0,0 +1,8 @@
+import { BadRequestException } from '@roxavn/core/share';
+import { baseModule } from './module';
+
+export class UserExistsException extends BadRequestException {
+  i18n = {
+    default: { key: 'Error.UserExistsException', ns: baseModule.escapedName },
+  };
+}
--- packages/module-user/src/share/index.ts
+++ packages/module-user/src/share/index.ts
@@ -3,3 +3,4 @@ export * from './permissions';
 export * from './routes';
 export * from './module';
 export * from './interfaces';
+export * from './errors';
