d6045e0db7eb71abc09fce239402e02ba96a6609
Toan Nguyen <woodpecker146@gmail.com>
2023-02-02 15:51:19 +0700

    feat(module-user): add addUserRoleApi

---
+++ packages/module-user/src/server/services/add.user.role.ts
@@ -0,0 +1,19 @@
+import { addUserRoleApi } from '../../share';
+import { UserRole } from '../entities';
+import { AuthApiService, InferAuthApiRequest } from '../middlerware';
+import { serverModule } from '../module';
+
+@serverModule.useApi(addUserRoleApi)
+export class AddUserRoleApiService extends AuthApiService<
+  typeof addUserRoleApi
+> {
+  async handle(request: InferAuthApiRequest<typeof addUserRoleApi>) {
+    const userRole = new UserRole();
+    userRole.ownerId = request.$user.id;
+    userRole.roleId = request.roleId;
+    if (request.scopeId) {
+      userRole.scopeId = request.scopeId;
+    }
+    await this.dbSession.save(userRole);
+  }
+}
--- packages/module-user/src/share/apis/index.ts
+++ packages/module-user/src/share/apis/index.ts
@@ -11,3 +11,4 @@ export * from './get.user.roles';
 export * from './get.stats.module.role';
 export * from './get.my.module.roles';
 export * from './get.module.roles';
+export * from './add.user.role';
