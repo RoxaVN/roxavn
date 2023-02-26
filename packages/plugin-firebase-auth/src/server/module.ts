69bab8abe62058f516b3f4c60bdddf9cf5b83385
woody <woodpecker146@gmail.com>
2023-02-26 14:24:35 +0700

    feat(plugin-firebase-auth): init module

---
+++ packages/plugin-firebase-auth/src/server/module.ts
@@ -0,0 +1,7 @@
+import { ServerModule } from '@roxavn/core/server';
+
+import { baseModule } from '../base';
+
+const serverModule = ServerModule.fromBase(baseModule);
+
+export { serverModule };
---
+++ packages/plugin-firebase-auth/src/web/module.ts
@@ -0,0 +1,6 @@
+import { WebModule } from '@roxavn/core/web';
+import { baseModule } from '../base';
+
+const webModule = WebModule.fromBase(baseModule);
+
+export { webModule };
