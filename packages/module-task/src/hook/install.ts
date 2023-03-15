9d4de7c3b7ee351367eeb25be3c29de406655b44
Toan Nguyen <woodpecker146@gmail.com>
2023-03-15 14:57:54 +0700

    feat(module-task): add install hook

---
+++ packages/module-task/src/hook/install.ts
@@ -0,0 +1,10 @@
+import { BaseService, hookManager } from '@roxavn/core/server';
+
+import { roles } from '../base';
+
+export class InstallHook extends BaseService {
+  async handle() {
+    await this.create(hookManager.createRoleService).handle(roles);
+    await this.create(hookManager.setAdminRoleService).handle(roles.Admin);
+  }
+}
