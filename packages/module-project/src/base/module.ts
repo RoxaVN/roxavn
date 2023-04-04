f162058ffc6de73d9de56ac07214886faa49e3a3
Toan Nguyen <woodpecker146@gmail.com>
2023-04-04 09:12:34 +0700

    refactor: change module-task to module-project

--- packages/module-project/src/base/module.ts
+++ packages/module-project/src/base/module.ts
@@ -1,5 +1,5 @@
 import { BaseModule } from '@roxavn/core/base';
 
-export const baseModule = new BaseModule('@roxavn/module-task', {
+export const baseModule = new BaseModule('@roxavn/module-project', {
   appPath: 'projects',
 });
