9dfcb464502b878afe1946d5954e15301ba6a97e
woody <woodpecker146@gmail.com>
2023-07-02 08:58:13 +0700

    chore: remove .web in packages

--- packages/.web/app/init.modules.ts
+++
@@ -1,9 +0,0 @@
-// init client modules
-
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// start block
-export { default as roxavn$module_utils } from '@roxavn/module-utils/web/init';
-export { default as roxavn$module_user } from '@roxavn/module-user/web/init';
-// end block
-
-export {};
