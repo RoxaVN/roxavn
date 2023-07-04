ff90cc013ba61b38a1852909186e13eff5428af0
woody <woodpecker146@gmail.com>
2023-07-04 14:59:37 +0700

    chore: update remix to 1.18.1

--- packages/dev-web/.web/remix.config.mjs
+++ packages/dev-web/.web/remix.config.mjs
@@ -7,7 +7,7 @@ const appConfig = {
   serverModuleFormat: 'esm',
   serverBuildPath: 'build/index.mjs',
   future: {
-    unstable_dev: true,
+    v2_headers: true,
     v2_routeConvention: true,
     v2_errorBoundary: true,
     v2_meta: true,
