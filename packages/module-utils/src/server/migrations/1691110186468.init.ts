f0dc4214bf0ed05f7a5701fef2ddfed78cce6d9f
woody <woodpecker146@gmail.com>
2023-08-04 07:57:10 +0700

    feat(module-utils): add migration

---
+++ packages/module-utils/src/server/migrations/1691110186468.init.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitModuleUtils1691110186468 implements MigrationInterface {
+  name = 'InitModuleUtils1691110186468';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE "setting" (
+        "id" SERIAL NOT NULL,
+        "module" character varying NOT NULL,
+        "name" character varying NOT NULL,
+        "type" character varying NOT NULL,
+        "metadata" jsonb NOT NULL,
+        "updatedDate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        CONSTRAINT "PK_fcb21187dc6094e24a48f677bed" PRIMARY KEY ("id")
+      )
+      `);
+    await queryRunner.query(`
+      CREATE UNIQUE INDEX "IDX_7716df862d864580e97a072565" ON "setting" ("module", "name")
+      `);
+    await queryRunner.query(`
+      CREATE TABLE "translation" (
+        "id" BIGSERIAL NOT NULL,
+        "key" character varying(256) NOT NULL,
+        "lang" character varying(16) NOT NULL,
+        "content" text NOT NULL,
+        "metadata" jsonb,
+        "updatedDate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        CONSTRAINT "PK_7aef875e43ab80d34a0cdd39c70" PRIMARY KEY ("id")
+      )
+      `);
+    await queryRunner.query(`
+      CREATE UNIQUE INDEX "IDX_15fe4a3b2ffc6adb98e897e681" ON "translation" ("key", "lang")
+      `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DROP INDEX "public"."IDX_15fe4a3b2ffc6adb98e897e681"
+      `);
+    await queryRunner.query(`
+      DROP TABLE "translation"
+      `);
+    await queryRunner.query(`
+      DROP INDEX "public"."IDX_7716df862d864580e97a072565"
+      `);
+    await queryRunner.query(`
+      DROP TABLE "setting"
+      `);
+  }
+}
---
+++ packages/module-utils/src/server/migrations/index.ts
@@ -0,0 +1,1 @@
+export * from './1691110186468.init.js';
