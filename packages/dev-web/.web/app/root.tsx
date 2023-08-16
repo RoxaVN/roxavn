b92c4f1ed934cf80173c88d1f79fe6daf699b01d
woody <woodpecker146@gmail.com>
2023-08-16 16:31:06 +0700

    chore(dev-web): add rootLoader and rootMeta

--- packages/dev-web/.web/app/root.tsx
+++ packages/dev-web/.web/app/root.tsx
@@ -1,21 +1,7 @@
-import { json, type LoaderArgs, type V2_MetaFunction } from '@remix-run/node';
-import { i18nextServer } from '@roxavn/core/server';
+import { rootLoader, rootMeta } from '@roxavn/dev-web/server';
 import { createRoot } from '@roxavn/dev-web/web';
 import * as initModules from './init.modules.js';
 
-export const loader = async ({ request }: LoaderArgs) => {
-  const locale = await i18nextServer.getLocale(request);
-  return json({ locale });
-};
-
-export const meta: V2_MetaFunction = () => [
-  {
-    charSet: 'utf-8',
-  },
-  {
-    name: 'viewport',
-    content: 'width=device-width,initial-scale=1',
-  },
-];
-
+export const loader = rootLoader;
+export const meta = rootMeta;
 export default createRoot({ initModules });
--- packages/dev-web/src/server/index.ts
+++ packages/dev-web/src/server/index.ts
@@ -1,2 +1,3 @@
 export * from './bootstrap.js';
 export * from './entry.js';
+export * from './root.js';
