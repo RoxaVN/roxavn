92e7b19471c9a369a9834a22ae6a59e70d26d2b3
woody <woodpecker146@gmail.com>
2023-08-23 11:27:36 +0700

    feat(module-web3): add Web3EventConsumersService

--- packages/module-web3/src/server/services/web3.event.consumer.ts
+++ packages/module-web3/src/server/services/web3.event.consumer.ts
@@ -1,8 +1,9 @@
 import { InferApiRequest } from '@roxavn/core/base';
 import { InjectDatabaseService } from '@roxavn/core/server';
+import { MoreThan } from 'typeorm';
 
 import { web3EventConsumerApi } from '../../base/index.js';
-import { Web3EventConsumer } from '../entities/index.js';
+import { Web3Event, Web3EventConsumer } from '../entities/index.js';
 import { serverModule } from '../module.js';
 
 @serverModule.useApi(web3EventConsumerApi.getMany)
@@ -22,5 +23,50 @@ export class GetWeb3EventConsumersApiService extends InjectDatabaseService {
       items: items,
       pagination: { page, pageSize, totalItems },
     };
+  }
+}
+
+@serverModule.injectable()
+export abstract class Web3EventConsumersService extends InjectDatabaseService {
+  abstract consume(event: Record<string, any>): Promise<void>;
+  abstract crawlerId: string;
+
+  async handle() {
+    let consumer = await this.entityManager
+      .getRepository(Web3EventConsumer)
+      .findOne({
+        lock: { mode: 'pessimistic_write' },
+        where: {
+          crawlerId: this.crawlerId,
+          name: this.constructor.name,
+        },
+      });
+    const fromBlock = consumer ? consumer.lastConsumeBlockNumber : '0';
+    const events = await this.entityManager.getRepository(Web3Event).find({
+      where: {
+        blockNumber: MoreThan(fromBlock),
+        crawlerId: this.crawlerId,
+      },
+      order: { blockNumber: 'ASC' },
+    });
+    if (events.length) {
+      for (const event of events) {
+        await this.consume(event.data);
+      }
+      // update consumer
+      const lastConsumeBlockNumber = events[events.length - 1].blockNumber;
+      if (consumer) {
+        consumer.lastConsumeBlockNumber = lastConsumeBlockNumber;
+        await this.entityManager.save(consumer);
+      } else {
+        consumer = new Web3EventConsumer();
+        consumer.crawlerId = this.crawlerId;
+        consumer.name = this.constructor.name;
+        consumer.lastConsumeBlockNumber = lastConsumeBlockNumber;
+        await this.entityManager
+          .getRepository(Web3EventConsumer)
+          .insert(consumer);
+      }
+    }
   }
 }
