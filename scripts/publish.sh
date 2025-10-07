f6e60b7957c3816db1d9313bee6a458cf5ac5206
Toan Nguyen <woodpecker146@gmail.com>
2025-10-07 07:59:21 +0700

    chore: update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 folders=(
-    "packages/cli"
     "packages/openapi"
-    "packages/module-utils"
     "packages/module-user"
+    "packages/module-system"
     "packages/module-upload"
     "packages/module-installer"
     "packages/module-message"
@@ -13,6 +12,9 @@ folders=(
     "packages/module-web3"
     "packages/module-asset"
     "packages/module-currency"
+    "packages/module-location"
+    "packages/plugin-sequence"
+    "packages/module-accounting"
     "packages/module-socket"
     "packages/module-game"
     "packages/module-referral"
@@ -24,22 +26,46 @@ folders=(
     "packages/module-skill"
     "packages/module-timesheet"
     "packages/module-organization"
+    "packages/module-finance-contract"
+    "packages/plugin-api-logger"
+    "packages/plugin-api-rate-limiter"
     "packages/plugin-otp"
     "packages/plugin-email-sender"
     "packages/plugin-auth-email"
     "packages/plugin-auth-web3"
+    "packages/plugin-auth-webauthn"
+    "packages/plugin-auth-two-factor"
+    "packages/plugin-identity-pin"
+    "packages/plugin-identity-totp"
+    "packages/plugin-auth-two-factor-totp"
+    "packages/plugin-reauth-password"
     "packages/plugin-payment"
     "packages/plugin-payment-web3"
     "packages/plugin-project-notification"
+    "packages/plugin-project-message"
     "packages/plugin-firebase"
     "packages/plugin-auth-firebase"
     "packages/plugin-asset-web3"
     "packages/plugin-asset-category"
+    "packages/plugin-user-ban"
     "packages/plugin-user-info"
     "packages/plugin-web3-airdrop"
     "packages/plugin-store-record"
     "packages/plugin-casino-game"
     "packages/plugin-telegram-bot"
+    "packages/plugin-message-report"
+    "packages/plugin-message-reaction"
+    "packages/plugin-message-pin"
+    "packages/plugin-message-media"
+    "packages/plugin-message-poll"
+    "packages/plugin-message-notification"
+    "packages/plugin-notification-in-app"
+    "packages/plugin-notification-push"
+    "packages/plugin-notification-push-web"
+    "packages/plugin-location-countries"
+    "packages/plugin-location-vietnam"
+    "packages/plugin-location-address"
+    "packages/plugin-news"
 )
 
 # Lặp qua từng thư mục
@@ -47,7 +73,10 @@ for dir in "${folders[@]}"; do
     if [ -d "$dir" ]; then
         echo "📂 Processing folder: $dir"
         cd "$dir"
-        npx standard-version --release-as 3.3.0
+        npx roxavn sync
+        npx roxavn-ts migration:up
+        npm run build
+        npx standard-version --release-as 3.6.0
         git push --follow-tags origin main && npm publish
         cd - > /dev/null
     else
